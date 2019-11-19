20e2ec2d4dd5c29d96aa3860331a7ce580e6674d
Robbie <20fishelro@fpsct.org>
2019-11-19 14:00:13 -0500

    Created new classes for 2 different activities: the activity to create a new team and to load a previously saved team. Finished coding the buttons & text on the main activity screen, however I am not 100% sure that everything is complete.

--- app/src/main/java/fhscapstone/fisheltestapp/LoadTeamList.java
+++ app/src/main/java/fhscapstone/fisheltestapp/LoadTeamList.java
@@ -1,22 +1,15 @@
 package fhscapstone.fisheltestapp;
 
-import android.content.Intent;
-import android.content.Context;
-import android.content.ContextWrapper;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.widget.Button;
-import android.widget.TextView;
+import android.view.View;
 
-public class MainActivity extends AppCompatActivity {
-
-    @Override
+public class LoadTeamList extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
@@ -54,22 +47,4 @@ public class MainActivity extends AppCompatActivity {
 
         return super.onOptionsItemSelected(item);
     }
-
-    public void infoScreen() {
-            Button newTeam = (Button) findViewById(R.id.newTeam);
-            newTeam.setOnClickListener(new View.OnClickListener() {
-                public void onClick (View v) {
-                    startActivity(new Intent(MainActivity.this, NewTeamActivity.class));
-                }
-
-                                       });
-            Button loadTeam = (Button) findViewById(R.id.loadTeam);
-            loadTeam.setOnClickListener(new View.OnClickListener() {
-                public void onClick (View v) {
-                    startActivity(new Intent(MainActivity.this, LoadTeamList.class));
-                }
-                                        });
-
-            TextView title = (TextView) findViewById(R.id.title);
-        }
-    }
+}
