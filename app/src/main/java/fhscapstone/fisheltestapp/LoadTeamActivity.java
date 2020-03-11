a8d53445e622cab8d93a6ad21acf0edb39e93717
Robbie <20fishelro@fpsct.org>
2020-03-10 21:53:48 -0400

    Completed code for LoadTeamActivity, will start writing code for TeamHomeActivity and the stat optimization algorithms. Should run tests to determine the weight of the different stats depending on coach priority and just general importance

--- app/src/main/java/fhscapstone/fisheltestapp/LoadTeamActivity.java
+++ app/src/main/java/fhscapstone/fisheltestapp/LoadTeamActivity.java
@@ -1,10 +1,18 @@
 package fhscapstone.fisheltestapp;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.ObjectInputStream;
 
 public class LoadTeamActivity extends Activity {
 
@@ -19,7 +27,55 @@ public class LoadTeamActivity extends Activity {
     }
 
     public void infoScreen() {
-        Button team1 = (Button)findByViewId(R.id.selection1);
+        Button team1 = (Button) findViewById(R.id.selection1);
+        Button team2 = (Button) findViewById(R.id.selection2);
+        Button team3 = (Button) findViewById(R.id.selection3);
+
+        TextView file1 = (TextView) findViewById(R.id.file1);
+        TextView file2 = (TextView) findViewById(R.id.file2);
+        TextView file3 = (TextView) findViewById(R.id.file3);
+
+        if (files[0].equals(null)) {
+            team1.setEnabled(false);
+            file1.setText("No Team");
+        } else {
+            team1.setText("Select");
+            file1.setText(getTeamFromFile(0).getName());
+        }
+
+        if (files[1].equals(null)) {
+            team2.setEnabled(false);
+            file2.setText("No Team");
+        } else {
+            team2.setText("Select");
+            file2.setText(getTeamFromFile(1).getName());
+        }
+
+        if (files[2].equals(null)) {
+            team3.setEnabled(false);
+            file3.setText("No Team");
+        } else {
+            team3.setText("Select");
+            file3.setText(getTeamFromFile(2).getName());
+        }
+
+        team1.setOnClickListener(new View.onClickListener() {
+            public void onClick(View v) {
+                startActivity(new Intent(LoadTeamActivity.this, TeamHomeActivity.class));
+            }
+        });
+
+        team2.setOnClickListener(new View.onClickListener() {
+            public void onClick(View v) {
+                startActivity(new Intent(LoadTeamActivity.this, TeamHomeActivity.class));
+            }
+        });
+
+        team3.setOnClickListener(new View.onClickListener() {
+            public void onClick(View v) {
+                startActivity(new Intent(LoadTeamActivity.this, TeamHomeActivity.class));
+            }
+        });
     }
 
     public static boolean addSaveFile(File f) {
@@ -33,5 +89,31 @@ public class LoadTeamActivity extends Activity {
             return false;
         }
         return true;
+    }
+
+    public static Team getTeamFromFile(int fileNo) {
+        FileInputStream f;
+        ObjectInputStream o;
+            try {
+                f = new FileInputStream(files[fileNo]);
+            } catch (FileNotFoundException e) {
+                System.out.println("Error: File Not Found");
+                return null;
+            }
+            try {
+                o = new ObjectInputStream(f);
+            } catch (IOException e) {
+                System.out.println("Error: File Not Found");
+                return null;
+            }
+            try {
+                return (Team) o.readObject();
+            } catch (IOException e) {
+                System.out.println("Error: File Not Found");
+                return null;
+            } catch (ClassNotFoundException e) {
+                System.out.println("Error: File Not Found");
+                return null;
+            }
     }
 }
